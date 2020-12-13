1e44790e774e61c3a6549878ffee3e3c758f1f91
someRandom@gmail.com <sabitovyusuf@gmail.com>
2020-12-13 21:10:13 +0500

    Database added

---
+++ app/src/main/java/com/example/vazifa/VazifaDataBase.java
@@ -0,0 +1,9 @@
+package com.example.vazifa;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class VazifaDataBase {
+}
