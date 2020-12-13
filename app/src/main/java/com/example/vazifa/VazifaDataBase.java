308042e7975f09a35f233319c8f6a595e2d71e6e
someRandom@gmail.com <sabitovyusuf@gmail.com>
2020-12-13 22:36:27 +0500

    added DB

--- app/src/main/java/com/example/vazifa/VazifaDataBase.java
+++ app/src/main/java/com/example/vazifa/VazifaDataBase.java
@@ -5,5 +5,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 
-public class VazifaDataBase {
+class VazifaDataBase extends SQLiteOpenHelper{
+
+    private static final String DB_name ="Main_DB"; //name of the database
+    private static int DB_version          =1;         //version
+
+    VazifaDataBase(Context context){
+        super(context,DB_name,null,DB_version);
+    };
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
