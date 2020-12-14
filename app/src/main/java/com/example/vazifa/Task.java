fb0130f3dfb386ad10fb5fd3b3846e65d8c17829
someRandom@gmail.com <sabitovyusuf@gmail.com>
2020-12-14 15:28:57 +0500

    added Task Class

---
+++ app/src/main/java/com/example/vazifa/Task.java
@@ -0,0 +1,62 @@
+package com.example.vazifa;
+
+//Класс в котором будет хранится данные о задачах
+
+
+public class Task {
+    private int    id;              // Идентификатор
+    private String name;            // Название задачи
+    private String description;     // Описание задачи
+    private String date;            // Дата когда задача было создана
+
+    public Task(int id, String name, String description, String date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+
+    public Task(){};
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
--- app/src/main/java/com/example/vazifa/VazifaDataBase.java
+++ app/src/main/java/com/example/vazifa/VazifaDataBase.java
@@ -8,15 +8,21 @@ import android.database.sqlite.SQLiteDatabase;
 class VazifaDataBase extends SQLiteOpenHelper{
 
     private static final String DB_name ="Main_DB"; //name of the database
-    private static int DB_version          =1;         //version
+    private static int DB_version          =1;      //version
 
+    //Конструктор Класса
     VazifaDataBase(Context context){
         super(context,DB_name,null,DB_version);
     };
 
+    //Главная таблица
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("Create Table Main("             //название таблицы Main
+                + "id integer primary key autoincrement,"
+                + "name text,"
+                + "description text,"
+                + "date date );");
     }
 
     @Override
