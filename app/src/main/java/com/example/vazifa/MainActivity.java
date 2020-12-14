09dfd35b2fb508f06280292f7defe7c7772e8444
Bogdan Mingboyev <mingboyev.kh@gmail.com>
2020-12-15 00:38:30 +0300

    vazifa-16, vazifa-15 done
    vazifa-14 bug found

--- app/src/main/java/com/example/vazifa/MainActivity.java
+++ app/src/main/java/com/example/vazifa/MainActivity.java
@@ -2,25 +2,32 @@ package com.example.vazifa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-
-
+public class MainActivity extends AppCompatActivity{
+    private FloatingActionButton btnAddTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        addListenerOnButtons();
     }
 
-
-
-
-
+    private void addListenerOnButtons() {
+        btnAddTask = (FloatingActionButton) findViewById(R.id.AddButton);
+        btnAddTask.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent AddTask = new Intent(MainActivity.this, AddTask_activity.class);
+                        startActivity(AddTask);
+                    }
+                }
+        );
+    }
 }
