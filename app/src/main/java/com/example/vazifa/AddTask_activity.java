79c3e9cd49049022eca95442941724f740c32ee5
someRandom@gmail.com <sabitovyusuf@gmail.com>
2020-12-15 09:47:41 +0500

    fixed issues

--- app/src/main/java/com/example/vazifa/AddTask_activity.java
+++ app/src/main/java/com/example/vazifa/AddTask_activity.java
@@ -3,12 +3,30 @@ package com.example.vazifa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddTask_activity extends AppCompatActivity {
+
+    FloatingActionButton saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtask);
+
+        saveButton=findViewById(R.id.SaveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //addd to data base
+            }
+        });
     }
+
+
+
+
 }
--- app/src/main/java/com/example/vazifa/MainActivity.java
+++ app/src/main/java/com/example/vazifa/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.vazifa;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
 
@@ -21,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
 }
+
+
+
+
+
+
+
+
+
